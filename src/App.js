e073d28879cbbe759b7e638662e1df90e0ae7b6c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-04-08 14:19:19 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/*
+
+  Your app must include:
+  - useState
+  - Event handling
+  - At least 2 buttons
+  - User input (for some apps)
+  - Conditional rendering
+  - Clean UI
+
+*/
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-green-600">ITPS205 Show/Hide Content App</h2>
+      <p>Group 3</p>
+    </div>
+  );
+}
